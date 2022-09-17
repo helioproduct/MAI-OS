e406697d49e776545afba6db80b0d02f14b8df0c
helio <helioproduct.sync@gmail.com>
2022-09-17 23:17:22 +0300

    lab1, lab2: sources added

---
+++ lab2/generate.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+from random import randint
+
+def generate_file(lines_amount : int, path_to_file : str):
+    pass
+
+
+def __main__():
+    path_to_file = int(int(input('Enter path to file')))
+    lines_amount = int(input('Enter lines amount: '))
+    generate_file(lines_amount, path_to_file)
+

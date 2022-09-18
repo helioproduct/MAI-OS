8b50630bc52bf3f1558546a7616ddc555b314644
helio <helioproduct.sync@gmail.com>
2022-09-18 15:35:49 +0300

    lab2: clean up

--- lab2/main.c
+++ lab2/main.c
@@ -1,1 +1,42 @@
 // Вариант 7
+
+#include <stdio.h>
+#include "unistd.h"
+
+double calclulate_sum()
+{
+    printf("%s", "pass...\n");
+}
+
+int main()
+{
+    int fildes[2];
+    
+    if (pipe(fildes) == -1) {
+        perror("pipe open error");
+        return -1;
+    }
+
+    int id = fork();
+
+    if (id == -1) {
+        perror("fork error");
+        return -2;
+    }
+    
+    if (id == 0) {
+        printf("%s\n", "child");
+        printf("pid = %d\n", getpid());
+        calclulate_sum();
+    }
+    else {
+
+        // open file to read
+        
+
+        printf("%s\n", "parent");
+        printf("%d\n", getpid());
+    }
+
+    return 0;
+}

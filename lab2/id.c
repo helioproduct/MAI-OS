50bd5f889865d0d1b1667aa09e38658e6a98ae99
helio <helioproduct.sync@gmail.com>
2022-09-25 17:41:17 +0300

    initial commit

--- lab2/id.c
+++ lab2/id.c
@@ -12,9 +12,13 @@ int main(void)
     }
     printf("Current ID: %d , parent's ID: %d\n", getpid(), 
                                                  getppid());
-    if (id != 0) {
-        // wait for child process
-        wait(NULL);
-    }
+    int res = wait(NULL);
+
+    if (res == -1) {
+        printf("No children to wait\n");
+    } else {
+        printf("%d finished execution\n", getpid());
+    } 
+    
     return 0;
 }
